f5c44f5ae581316eef934c8d44d70f364bc9a806
techtobit <nurthern2016@gmail.com>
2024-05-02 10:47:12 +0600

    updated hero page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,14 +5,11 @@ import Link from 'next/link';
 
 const Page = () => {
   return (
-    <div>
-      <Link href="/auth">
-        Auth
-      </Link>
-      <Link href="/Login">
+    <div className=' h-screen pattern-topography flex items-center justify-center gap-6 '>
+      <Link className='px-5 py-2 text-white rounded-full bg-blue-600 hover:bg-blue-500' href="/Login">
         Login
       </Link>
-      <Link href="/Dashboard">
+      <Link  className='px-5 py-2 text-white rounded-full bg-blue-600 hover:bg-blue-500' href="/Dashboard">
       Dashboard
       </Link>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@lorenzopalaia/tailwind-hero-patterns'),
+  ],
 };
